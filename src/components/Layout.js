6c1378e8d8b098487e74a1b859c41f3c109f719b
Willy Liu <willyjliu@gmail.com>
2019-11-26 18:51:42 +0100

    Add header

---
+++ src/components/Layout.js
@@ -0,0 +1,8 @@
+import "normalize.css"
+import "./global.css"
+
+function Layout({ children }) {
+  return children
+}
+
+export default Layout
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,14 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import VeloxLogo from "../images/logo.png"
+import styles from "./Header.module.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+function Header() {
+  return (
+    <div className={styles.container}>
+      <img className={styles.logo} src={VeloxLogo} alt="Velox logo" />
+      <p>Short description of the game</p>
     </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  )
 }
 
 export default Header
