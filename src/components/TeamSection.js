97fff904a311cf461c7d030648232ccc11bfdb8c
Willy Liu <willyjliu@gmail.com>
2019-11-27 13:43:03 +0100

    Use graphql for images

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -1,50 +1,88 @@
 import React from "react"
 import Section from "./Section"
 import styles from "./TeamSection.module.css"
-
-import JoelImg from "../images/joel.jpg"
-import BenImg from "../images/ben.jpg"
-import AlexImg from "../images/alex.jpg"
-import JohnImg from "../images/john.jpg"
-import WillyImg from "../images/willy.jpg"
-
-const team = [
-  {
-    name: "Joel Thunander",
-    role: "Project leader, developer",
-    img: JoelImg,
-  },
-  {
-    name: "Ben Forsrup",
-    role: "Developer",
-    img: BenImg,
-  },
-  {
-    name: "Álex Pérez",
-    role: "Developer",
-    img: AlexImg,
-  },
-  {
-    name: "John Forsgren",
-    role: "Developer",
-    img: JohnImg,
-  },
-  {
-    name: "Willy Liu",
-    role: "Art/design, web developer",
-    img: WillyImg,
-  },
-]
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 function TeamSection() {
+  // First time using gatsby-image, there has to be a more efficient
+  // way to do this...
+  const data = useStaticQuery(graphql`
+    query ImgQuery {
+      joel: file(relativePath: { eq: "team/joel.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      ben: file(relativePath: { eq: "team/ben.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      alex: file(relativePath: { eq: "team/alex.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      john: file(relativePath: { eq: "team/john.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      willy: file(relativePath: { eq: "team/willy.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  const team = [
+    {
+      name: "Joel Thunander",
+      role: "Project leader, developer",
+      img: data.joel,
+    },
+    {
+      name: "Ben Forsrup",
+      role: "Developer",
+      img: data.ben,
+    },
+    {
+      name: "Álex Pérez",
+      role: "Developer",
+      img: data.alex,
+    },
+    {
+      name: "John Forsgren",
+      role: "Developer",
+      img: data.john,
+    },
+    {
+      name: "Willy Liu",
+      role: "Art/design, web developer",
+      img: data.willy,
+    },
+  ]
+
   return (
     <Section name={"The team"}>
       <div className={styles.membersGrid}>
         {team.map(member => (
-          <div className={styles.member}>
-            <img
+          <div className={styles.member} key={member.name}>
+            <Img
               className={styles.memberImg}
-              src={member.img}
+              fluid={member.img.childImageSharp.fluid}
               alt={member.name}
             />
             <div className={styles.memberName}>{member.name}</div>
