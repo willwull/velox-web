3a04af89de084cd2d5bf03dd5d3df520e0e79b3a
Willy Liu <willyjliu@gmail.com>
2019-11-28 09:33:32 +0100

    Add more info in About section

--- src/components/AboutGameSection.js
+++ src/components/AboutGameSection.js
@@ -24,6 +24,40 @@ function AboutGameSection() {
         ranging from simple Death Match to our own custom made mode called Base
         Shooting.
       </p>
+
+      <div className={styles.features}>
+        <div>
+          <h2>Fun gameplay</h2>
+          <p>
+            Velox is suitable both as a casual multiplayer game for short
+            sessions or even a whole evening with your friends.
+          </p>
+        </div>
+
+        <div>
+          <h2>Every match is unique</h2>
+          <p>
+            Velox has many game modes and also different maps with unique
+            gameplay mechanics such jetpacks.
+          </p>
+        </div>
+
+        <div>
+          <h2>Free to play version</h2>
+          <p>
+            Velox has a free to play version containing two game modes and a
+            handful of maps so anyone can play!
+          </p>
+        </div>
+
+        <div>
+          <h2>Cross platform</h2>
+          <p>
+            Velox will be available on PC (Windows), Nintendo Switch,
+            PlayStation 4 and Xbox One.
+          </p>
+        </div>
+      </div>
     </Section>
   )
 }
